7805f1fb72dfe7fc1c8eb3d25bc26370c013d0da
Christian Mund <git@mundpropaganda.net>
2019-11-13 00:11:26 +0100

    utils/getExtensionsInfo: fetch directly from updateUrls

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,60 @@
+const parser = new DOMParser()
+
+const addIfNew = (arr = [], item) =>
+  item === undefined ? arr : [...new Set([...arr]).add(item)]
+
+const fetchExtensionInfo = async (updateUrl, ids, prodversion) => {
+  const x = ids.map(id => `x=${encodeURIComponent(`id=${id}&uc`)}`)
+
+  const txt = await fetch(
+    `${updateUrl}?${x.join('&')}&prodversion=${prodversion}`
+  ).then(req => req.text())
+
+  const xml = parser.parseFromString(txt, 'text/xml')
+
+  return Array.from(xml.querySelectorAll('app')).map(el => {
+    const updatecheck = el.querySelector('updatecheck')
+
+    const info = {
+      id: el.getAttribute('appid'),
+      prodversion,
+      timestamp: new Date().getTime(),
+      updateUrl
+    }
+
+    return updatecheck
+      ? {
+          ...info,
+          ...updatecheck.getAttributeNames().reduce(
+            (acc, attr) => ({
+              ...acc,
+              [attr]: updatecheck.getAttribute(attr)
+            }),
+            {}
+          )
+        }
+      : info
+  })
+}
+
+const fetchExtensionsInfo = async (extensions, prodversion) => {
+  const jobs = extensions.reduce((acc, { id, updateUrl }) => {
+    if (updateUrl) {
+      acc[updateUrl] = addIfNew(acc[updateUrl], id)
+    }
+    return acc
+  }, {})
+
+  const data = await Promise.all(
+    Object.keys(jobs).map(
+      updateUrl =>
+        updateUrl && fetchExtensionInfo(updateUrl, jobs[updateUrl], prodversion)
+    )
+  )
+
+  return data.flat()
+}
+
 export const getConfig = () =>
   new Promise(resolve =>
     chrome.management.getAll(extensions =>
@@ -26,13 +83,4 @@ export const getExtensionsInfo = currentVersion =>
         }))
       )
     )
-  ).then(extensions =>
-    fetch('https://chrome-extension-service-git-errorlogs.kkkrist.now.sh/api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        prodversion: currentVersion,
-        extensions
-      })
-    }).then(res => res.json())
-  )
+  ).then(extensions => fetchExtensionsInfo(extensions, currentVersion))
